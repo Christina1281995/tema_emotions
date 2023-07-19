0221a197eb3c96f29d20fbee54ee1bf29a6318e0
Christina1281995 <christina.zorenboehmer@t-online.de>
2023-07-19 10:17:09 +0200

    updates to login

--- app.py
+++ app.py
@@ -114,6 +114,9 @@ with open('config.json') as f:
 st.title('Aspect-Based Emotion Labeling')
 
 # Set initial state
+if "login" not in st.session_state:
+    st.session_state["login"] = False
+
 if "start" not in st.session_state:
     st.session_state["start"] = False
 
@@ -123,7 +126,7 @@ if "expander" not in st.session_state:
 user_ids = [i["name"] for i in config["users"]]
 
 # For logging in
-if st.session_state["start"] == False:
+if st.session_state["login"] == False:
 
     # Prompt for user name
     user_name = st.text_input('Please enter your username')
@@ -139,107 +142,115 @@ if st.session_state["start"] == False:
                 if user["name"] == user_name:
                     
                     st.session_state.user_id = str(user_name)
-                    st.button("Start Labeling")
+                    st.button("Login")
+                    st.session_state["login"] = True
                     st.session_state["start"] = True
 
                 else:
+                    st.session_state["login"] = True
                     st.write("Username not found.")
     else:
+        st.session_state["login"] = True
         st.write('Please enter a username.')
                     
 
 # If session start is True
-else:
-     
-    # Get database data on user
-    user_data = get_user_data(st.session_state.user_id)
+if st.session_state["login"] == True:
 
-    # If user has already labelled, return the current question number
-    if user_data is not None:
-        question_number = user_data[2] # - 1  # Assuming the 'tweet_id' column is the third column in the table
+    # get current question number and data
+    if st.session_state["start"] == False:
+        # Get database data on user
+        user_data = get_user_data(st.session_state.user_id)
 
-    # If user hasn't done any labelling yet, set question number to 0
-    else:
-        # User is new, initialize question number to 0
-        question_number = 0
+        # If user has already labelled, return the current question number
+        if user_data is not None:
+            question_number = user_data[2] # - 1  # Assuming the 'tweet_id' column is the third column in the table
 
-    # Set question number
-    st.session_state["q_num"] = []
-    st.session_state["emotions"] = []
-    st.session_state["question_number"] = question_number
-
-    # Get pre-loaded data that is assigned to the username
-    if config["predefined"]:
-        path = [j["data_path"] for j in config["users"] if j["name"] == st.session_state.user_id][-1]
-        df = pd.read_csv(path)
-
-    # else:
-    #     with st.expander("Upload data", expanded=st.session_state.expander):
-    #         # load data 
-    #         uploaded_data = st.file_uploader("Csv file", type = ['.csv'])
-    #         df = load_data(uploaded_data)
-    #         st.info("Upload data")
-    #         st.session_state.expander = False
-
-    # If there is data
-    if df is not None:
-        
-        # Show current progress
-        percentage_progress = round((int(st.session_state.question_number) / len(df)) * 100)
-        if percentage_progress < 100:
-            st.progress(percentage_progress)
+        # If user hasn't done any labelling yet, set question number to 0
         else:
-            st.progress(100)
+            # User is new, initialize question number to 0
+            question_number = 0
 
-        # If we haven't reached the end of the labeling task yet
-        if st.session_state.question_number < len(df):
+        # Set question number
+        st.session_state["q_num"] = []
+        st.session_state["emotions"] = []
+        st.session_state["question_number"] = question_number
+        st.session_state["start"] = True
+        st.button("Start labeling")
+
+    else:
+        # Get pre-loaded data that is assigned to the username
+        if config["predefined"]:
+            path = [j["data_path"] for j in config["users"] if j["name"] == st.session_state.user_id][-1]
+            df = pd.read_csv(path)
+
+        # else:
+        #     with st.expander("Upload data", expanded=st.session_state.expander):
+        #         # load data 
+        #         uploaded_data = st.file_uploader("Csv file", type = ['.csv'])
+        #         df = load_data(uploaded_data)
+        #         st.info("Upload data")
+        #         st.session_state.expander = False
+
+        # If there is data
+        if df is not None:
             
-            # Set labeling parameters
-            # These are the parameters that will be shown upon submission (i.e. for the next round)
-            sentence = df["Sentence"][st.session_state.question_number]
-            aspect_term = df["Aspect Terms"][st.session_state.question_number]
-            sentiment = df["Sentiment"][st.session_state.question_number]
-
-            # These are the parameters to submit when button hits submit (i.e. the parameters currently shown --> index -1)
-            if st.session_state.question_number != 0:
-                prev_sentence = df["Sentence"][st.session_state.question_number - 1]
-                prev_aspect_term = df["Aspect Terms"][st.session_state.question_number - 1]
-                prev_sentiment = df["Sentiment"][st.session_state.question_number - 1]
-            # In the very first round (index = 0) use the current sentence
+            # Show current progress
+            percentage_progress = round((int(st.session_state.question_number) / len(df)) * 100)
+            if percentage_progress < 100:
+                st.progress(percentage_progress)
             else:
-                prev_sentence = df["Sentence"][st.session_state.question_number]
-                prev_aspect_term = df["Aspect Terms"][st.session_state.question_number]
-                prev_sentiment = df["Sentiment"][st.session_state.question_number]
+                st.progress(100)
 
-            # Highlight aspect term in the sentence           
-            def highlight_aspect_term(sentence, aspect_term):
-                aspect_term_pattern = re.escape(aspect_term)
-                pattern = r"(?<![^\W_])" + aspect_term_pattern + r"(?![^\W_])"
-                sentence_highlighted = re.sub(pattern, r"<span style='color:red'>\g<0></span>", sentence, flags=re.IGNORECASE)
-                return sentence_highlighted
+            # If we haven't reached the end of the labeling task yet
+            if st.session_state.question_number < len(df):
+                
+                # Set labeling parameters
+                # These are the parameters that will be shown upon submission (i.e. for the next round)
+                sentence = df["Sentence"][st.session_state.question_number]
+                aspect_term = df["Aspect Terms"][st.session_state.question_number]
+                sentiment = df["Sentiment"][st.session_state.question_number]
 
-            sentence_highlight =  highlight_aspect_term(sentence, aspect_term)
+                # These are the parameters to submit when button hits submit (i.e. the parameters currently shown --> index -1)
+                if st.session_state.question_number != 0:
+                    prev_sentence = df["Sentence"][st.session_state.question_number - 1]
+                    prev_aspect_term = df["Aspect Terms"][st.session_state.question_number - 1]
+                    prev_sentiment = df["Sentiment"][st.session_state.question_number - 1]
+                # In the very first round (index = 0) use the current sentence
+                else:
+                    prev_sentence = df["Sentence"][st.session_state.question_number]
+                    prev_aspect_term = df["Aspect Terms"][st.session_state.question_number]
+                    prev_sentiment = df["Sentiment"][st.session_state.question_number]
 
-            st.markdown(f"**Sentence:** {sentence_highlight}", unsafe_allow_html=True)
-            st.markdown(f"**Aspect Term:** {aspect_term}")
-            # st.markdown(f"**Sentiment:** {sentiment}")  # Leave out sentiment to not bias labeller 
+                # Highlight aspect term in the sentence           
+                def highlight_aspect_term(sentence, aspect_term):
+                    aspect_term_pattern = re.escape(aspect_term)
+                    pattern = r"(?<![^\W_])" + aspect_term_pattern + r"(?![^\W_])"
+                    sentence_highlighted = re.sub(pattern, r"<span style='color:red'>\g<0></span>", sentence, flags=re.IGNORECASE)
+                    return sentence_highlighted
 
-            form_key = "my_form"
-            with st.form(key=form_key):
-                options = EMOTION_OPTIONS
-                emotion = st.radio(
-                    'Assign an emotion to the aspect phrase', 
-                    options, 
-                    index=4, 
-                    format_func=lambda x: x[1])
+                sentence_highlight =  highlight_aspect_term(sentence, aspect_term)
 
-                if st.form_submit_button("Submit"): 
-                    emotion_to_add = emotion[0]
-                    data = [[st.session_state.question_number, emotion_to_add, prev_sentence, prev_aspect_term, prev_sentiment]]
-                    print(data)
-                    save_results(pd.DataFrame(data, columns=["q_num", "emotions", "sentence", "aspect_term", "sentiment"]))
-                    
-            st.write("---")
+                st.markdown(f"**Sentence:** {sentence_highlight}", unsafe_allow_html=True)
+                st.markdown(f"**Aspect Term:** {aspect_term}")
+                # st.markdown(f"**Sentiment:** {sentiment}")  # Leave out sentiment to not bias labeller 
+
+                form_key = "my_form"
+                with st.form(key=form_key):
+                    options = EMOTION_OPTIONS
+                    emotion = st.radio(
+                        'Assign an emotion to the aspect phrase', 
+                        options, 
+                        index=4, 
+                        format_func=lambda x: x[1])
+
+                    if st.form_submit_button("Submit"): 
+                        emotion_to_add = emotion[0]
+                        data = [[st.session_state.question_number, emotion_to_add, prev_sentence, prev_aspect_term, prev_sentiment]]
+                        print(data)
+                        save_results(pd.DataFrame(data, columns=["q_num", "emotions", "sentence", "aspect_term", "sentiment"]))
+                        
+                st.write("---")
 
         else:
             st.markdown("End of data.")
