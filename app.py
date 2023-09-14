a23c2e9ba4d41e9780e538c822287e134bfb848a
Christina1281995 <christina.zorenboehmer@t-online.de>
2023-09-14 10:09:35 +0200

    sidebar update

--- app.py
+++ app.py
@@ -238,6 +238,7 @@ else:                                                                  # If sess
                 </span>
                 <br><br>
                 {text}
+                <br>
                 """, unsafe_allow_html=True)
                 
                 for link in str(photo_url).split(','):
