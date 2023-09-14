4a4d7364eed5fb2a9d1bae1715be9efb3066b123
Christina1281995 <christina.zorenboehmer@t-online.de>
2023-09-14 16:34:54 +0200

    next button

--- app.py
+++ app.py
@@ -305,7 +305,7 @@ else:                                                                  # If sess
                     st.write("---")
                     st.markdown("  ")
                     
-                    if st.form_submit_button("Submit"):
+                    if st.form_submit_button("Next"):
                         if output_one:
                             target_one = json.dumps(output_one)
                         else:
